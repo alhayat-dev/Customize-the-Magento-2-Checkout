3278beed9db1497c621e16449f076fb62be1ba23
alhayatt-dev <alhayat.freelancer@gmail.com>
2022-09-19 01:44:11 +0500

    After adding customer email field in custom checkout-step ,the next button is not showing.

---
+++ CustomCheckout/view/frontend/web/js/view/email.js
@@ -0,0 +1,44 @@
+define([
+    'uiComponent',
+    'ko',
+    'Magento_Checkout/js/model/step-navigator',
+    'mage/translate',
+    'underscore',
+    'Magento_Checkout/js/model/quote'
+], function (
+    Component,
+    ko,
+    stepNavigator,
+    $t,
+    _,
+    quote
+){
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            template: 'Macademy_CustomCheckout/email',
+            isVisible: ko.observable(false)
+        },
+        quoteIsVirtual: quote.isVirtual(),
+        initialize: function () {
+            this._super();
+
+            stepNavigator.registerStep(
+                'email',
+                null,
+                $t('Email'),
+                this.isVisible,
+                _.bind(this.navigate, this),
+                this.sortOrder
+
+            );
+        },
+        navigate: function (){
+            this.isVisible(true);
+        },
+        navigateToNextStep: function () {
+            stepNavigator.next();
+        }
+    });
+});
